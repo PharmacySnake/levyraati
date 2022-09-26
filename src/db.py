0ab5e34ff7864a268616b2475ddb46c532479be0
PharmacySnake <andreas.katsaras@gmail.com>
2022-09-26 18:13:56 +0300

    Trying to fix heroku #4

--- src/db.py
+++ src/db.py
@@ -7,5 +7,6 @@ from os import getenv
 uri = getenv("DATABASE_URL")
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = uri
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
