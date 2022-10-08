ca3608420be351d9591c26879cff01ce9131fa79
PharmacySnake <andreas.katsaras@gmail.com>
2022-10-08 06:02:37 +0300

    Playing with how to display grades #6

--- src/routes.py
+++ src/routes.py
@@ -131,28 +131,28 @@ def sort_albums():
     #albums = []
     sort = request.args.get("sort")
     albums = album_repo.display_albums_asc()
-    frade = int(albums[3]*2)
+    frade = int(albums[3])
     if sort == "albums_desc":
       albums = album_repo.display_albums_desc()
-      frade = int(albums[3]*2)
+      frade = int(albums[3])
     elif sort == "albums_asc":
       albums = album_repo.display_albums_asc()
-      frade = int(albums[3]*2)
+      frade = int(albums[3])
     elif sort == "artists_desc":
       albums = album_repo.display_artists_desc()
-      frade = int(albums[3]*2)
+      frade = int(albums[3])
     elif sort == "artists_asc":
       albums = album_repo.display_artists_asc()
-      frade = int(albums[3]*2)
+      frade = int(albums[3])
     elif sort == "dates_desc":
       albums = album_repo.display_date_desc()
-      frade = int(albums[3]*2)
+      frade = int(albums[3])
     elif sort == "dates_asc":
       albums = album_repo.display_date_asc()
-      frade = int(albums[3]*2)
+      frade = int(albums[3])
     elif sort == "grades_desc":
       albums = album_repo.display_rating_desc()
-      frade = int(albums[3]*2)
+      frade = int(albums[3])
     elif sort == "grades_asc":
       albums = album_repo.display_rating_desc()
       print("album list:",albums)
