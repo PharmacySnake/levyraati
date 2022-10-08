08623ea78bcc293eea8042e5bb2ddc3704a721d0
PharmacySnake <andreas.katsaras@gmail.com>
2022-10-08 23:05:56 +0300

    Add album cover images to /home #14

--- src/routes.py
+++ src/routes.py
@@ -9,14 +9,14 @@ from services import user_serv
 def home():
   albums = album_repo.display_albums_home()
   #imagee = albums[1].cover_img
-  newalbums = []
+  newalbums = albums[:]
 
   for i in range(len(albums)):
     for j in range(len(albums[i])):
       if j != 4:
-        newalbums[i][j].append(albums[i][j])
+        newalbums[i][j] = albums[i][j]
       else: 
-        newalbums[i][j].append(b64encode(albums[i][4]).decode('utf-8'))
+        newalbums[i][j] = b64encode(albums[i][4]).decode('utf-8')
   #imagee = b64encode(data).decode('utf-8')
   return render_template("home.html", albums=newalbums)#, imagee=imagee)
 
