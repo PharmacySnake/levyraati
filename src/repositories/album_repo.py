4fe28f422c630837e6333e00c3c22d26978efa77
PharmacySnake <andreas.katsaras@gmail.com>
2022-10-09 23:30:37 +0300

    Add ability to give songs thumbs up/down. #25

--- src/repositories/album_repo.py
+++ src/repositories/album_repo.py
@@ -30,6 +30,7 @@ def add_album(artist:str, album_name:str, release_year:int, genre:str, comment:s
 
 def get_album_by_id(album_id:int):
   #values = {"id":album_id}
+  album_id = 5
   sql = "SELECT A.user_id, A.date_added, A.artist, A.album_name, A.release_year, A.genre " \
         "FROM albums A " \
         "WHERE A.id = :album_id"
