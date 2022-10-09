27533bac1f9a515be38b73ea9c6e17d252b89f98
PharmacySnake <andreas.katsaras@gmail.com>
2022-10-10 02:20:53 +0300

    Add ability to give songs thumbs up/down. #28

--- src/repositories/album_repo.py
+++ src/repositories/album_repo.py
@@ -1,4 +1,5 @@
 from db import db
+from sqlalchemy.sql import text
 import services.user_serv as user_serv
 from repositories import image_repo, review_repo, song_repo
 
@@ -30,12 +31,21 @@ def add_album(artist:str, album_name:str, release_year:int, genre:str, comment:s
 
 def get_album_by_id(album_id:int):
   values = {"id":album_id}
+  """
   sql = "SELECT A.user_id, A.date_added, A.artist, A.album_name, " \
                "A.release_year, A.genre " \
         "FROM albums A " \
         "WHERE A.id=:album_id"
+  """
+  sql = """
+        SELECT A.user_id, A.date_added, A.artist, A.album_name,
+               A.release_year, A.genre
+        FROM albums A
+        WHERE A.id=:album_id
+        """
   #result = db.session.execute(sql, {"id":album_id})
-  result = db.session.execute(sql, values)
+  #result = db.session.execute(sql, values)
+  result = db.session.execute(text(sql), values)
   return result.fetchone()[0]
   #return result.fetchall()
 
